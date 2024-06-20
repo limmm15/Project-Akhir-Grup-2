c6d9025e5a0e87825f62e97a939831f612cfe020
Sugestt <160681921+Sugestt@users.noreply.github.com>
2024-06-20 19:42:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['UPLOAD_FOLDER'] = 'uploads/photos'
 app.config['MAX_CONTENT_PATH'] = 16 * 1024 * 1024  # 16 MB max upload size
 
+#app.config['UPLOAD_FOLDER'] = 'uploads'
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
@@ -64,6 +67,43 @@ def pengumuman():
 @app.route('/daftar',methods=['GET','POST'])
 def daftar():
     return render_template('daftar.html')
+
+#Daftar
+#@app.route('/submit', methods=['POST'])
+#def submit():
+#    nama = request.form['nama']
+#    nama_orang_tua = request.form['nama_orang_tua']
+#    alamat = request.form['alamat']
+#
+#    kartu_keluarga = request.files['kartu_keluarga']
+#    akte_kelahiran = request.files['akte_kelahiran']
+#    ijazah_tk = request.files['ijazah_tk']
+#
+#    filenames = {}
+#    
+#    if kartu_keluarga:
+#        filename = os.path.join(app.config['UPLOAD_FOLDER'], kartu_keluarga.filename)
+#        kartu_keluarga.save(filename)
+#        filenames['kartu_keluarga'] = filename
+#    
+#    if akte_kelahiran:
+#        filename = os.path.join(app.config['UPLOAD_FOLDER'], akte_kelahiran.filename)
+#        akte_kelahiran.save(filename)
+#        filenames['akte_kelahiran'] = filename
+#    
+#    if ijazah_tk:
+#        filename = os.path.join(app.config['UPLOAD_FOLDER'], ijazah_tk.filename)
+#        ijazah_tk.save(filename)
+#        filenames['ijazah_tk'] = filename
+#
+#    db.form.insert_one({
+#        "nama": nama,
+#        "nama_orang_tua": nama_orang_tua,
+#        "alamat": alamat,
+#        "files": filenames
+#    })
+#
+#    return redirect(url_for('form', success=True))
 
 @app.route('/loginA',methods=['GET','POST'])
 def loginA():
